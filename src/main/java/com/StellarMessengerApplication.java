f491a3ec7d0c15ea80a1dc11d4e4de2fe531a393
Vernon Johnson <vtj2105@columbia.edu>
2018-02-17 23:06:21 -0500

    Initial commit

---
+++ src/main/java/com/StellarMessengerApplication.java
@@ -0,0 +1,15 @@
+package com;
+
+import com.Message;import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * Created by vernonjohnson23 on 2/17/18.
+ */
+@SpringBootApplication
+public class StellarMessengerApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(com.StellarMessengerApplication.class, args);
+    }
+}
